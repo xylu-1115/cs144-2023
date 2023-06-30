20e362e622e97bf854445711b32073a72b9c98e3
lxy <lxy1115@sjtu.edu.cn>
2023-06-30 15:38:52 +0800

    finish lab2 wrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,19 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  static const uint64_t two32 = 1ULL << 32;
+  static const uint64_t two31 = 1ULL << 31;
+  const uint64_t pos = (checkpoint >> 32 << 32) + (raw_value_ - zero_point.raw_value_);
+  if (pos > checkpoint + two31 && pos >= two32) {
+    return pos - two32;
+  } else if (pos < checkpoint - two31 && checkpoint >= two31) {
+    return pos + two32;
+  } else {
+    return pos;
+  }
 }
